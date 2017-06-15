171b4250d1669d85d2f3a87c4d8b21acdacf049f
Alexei Darmin <alexei.darmin@gmail.com>
2017-06-14 20:04:23 -0400

    Creep serialized autospawn and reassign

--- main.js
+++ main.js
@@ -1,34 +1,87 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleHarvester = require('role.harvester.js')
+var roleBuilder = require('role.builder.js')
+var roleUpgrader = require('role.upgrader.js')
 
 module.exports.loop = function () {
 
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
+  basicCreepFactory()
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
+  constructExtensions()
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
+  if (isStorageFull) reassignHarvesters()
+
+  for (let name in Game.creeps) {
+    let creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
     }
+    if (creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
+    }
+  }
+}
+
+function basicCreepFactory() {
+  let creepMade = false
+  let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester')
+  if (harvesters.length < 2) {
+    addBasicCreep('harvester')
+    creepMade = true
+  }
+
+  if (!creepMade) {
+    let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader')
+    if (upgraders.length < 2) {
+      addBasicCreep('upgrader')
+      creepMade = true
+    }
+    if (!creepMade) {
+      let builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder')
+      if (builders.length < 2) {
+        addBasicCreep('builder')
+        creepMade = true
+      }
+    }
+  }
+
+
+}
+
+function addBasicCreep(creepType) {
+  let creeps = _.filter(Game.creeps, (creep) => creep.memory.role == creepType)
+  console.log(creepType + ' ' + creeps.length)
+
+  if (creeps.length < 2) {
+    let newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, { role: creepType })
+    console.log('Spawning new ' + creepType + ' ' + newName)
+  }
+}
+
+function constructExtensions() {
+  let spawnPos = Game.spawns['Spawn1'].pos
+
+  // Get total number of extensions existing
+  let extensionPos = Game.rooms.sim.getPositionAt(spawnPos.x + 4, spawnPos.y + 4);
+
+  Game.rooms.sim.createConstructionSite(extensionPos, STRUCTURE_EXTENSION)
+
+  // Game.rooms.sim.createConstructionSite(10, 15, Extension);
+
+  return true
+}
+
+function isStorageFull() {
+  return Room.energyAvailable == Room.energyCapacityAvailable
+}
+
+function reassignHarvesters() {
+  for (let name in Game.creeps) {
+    const creep = Game.creeps[name]
+    if (creep.memory.role == 'harvester') {
+      creep.memory.role = 'builder'
+    }
+  }
 }
